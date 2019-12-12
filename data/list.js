2f744dea303114fc282d9c7dfb9f221b5a4af88b
Ronak Patel <ronak0624@gmail.com>
2019-12-12 18:23:08 +0000

    added template product page, script for populating pages

--- data/list.js
+++ data/list.js
@@ -1,2 +1,16 @@
 const products = require("./products");
-const fs = require("fs")
+const fs = require("fs");
+
+for(i=0;i<products.length;i++){
+    let currentWord = products[i].productTitle
+    var ans = "";
+    for(j=0;j<currentWord.length; j++){
+        if(currentWord[j] === " "){
+            ans += "%20"
+        }
+        else{ans += currentWord[j]}
+    }
+    fs.writeFile("data/seed/" + ans + ".html", ans + " Page", function(err){
+        throw err;
+    })
+}
--- routes/products.js
+++ routes/products.js
@@ -1,9 +1,10 @@
 const router = require("express").Router();
 const path = require("path");
 
+var test = "product.html"
 // Matches with "/products"
 router.route("/").get(function (req, res) {
-  res.sendFile(path.join(__dirname, "../views/product.html"))
+  res.sendFile(path.join(__dirname, "../views/" + test))
   console.log("Products route hit")
 });
 
